c4b84c9b201ac0ad88578088ec5bf1d512b0f452
joaognx <joaotutors291@gmail.com>
2026-01-18 18:01:04 -0300

    att produtos

--- routes/produtosRoutes.js
+++ routes/produtosRoutes.js
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client'
 const router = express.Router()
 const prisma = new PrismaClient()
 
-console.log("Modelos disponíveis no Prisma:", Object.keys(prisma))
+
 
 router.get('/', async (req, res) => {
     try {
@@ -22,6 +22,24 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) =>{
+    const { id } = req.params
+    try{
+        const produto = await prisma.produto.findUnique({
+        where: {
+                id: id 
+            },
+            include: {
+                category: true 
+            }
+            })
+
+        res.status(200).json(produto)
+    }catch(error) {
+        res.status(500).json({ error: "Erro ao buscar produto"})
+        console.log(id)
+    }
+})
 
 router.post('/', async (req, res) => {
     try {
